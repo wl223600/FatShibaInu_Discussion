b8835084b9f88dc4982234e2c0e5990ba4a595df
wl223600 <wl223600@gmail.com>
2024-08-14 23:38:13 +0800

    New Article Added

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,1 +1,2 @@
 hexo.extend.injector.register('body_end', `<script src="/js/outdate.js"></script>`,'post')
+hexo.extend.injector.register('head_end', `<link rel="stylesheet" href="/css/admonition.css">`)
--- source/js/outdate.js
+++ source/js/outdate.js
@@ -1,7 +1,7 @@
 (function() {
     //不同的日期显示不同的样式，200 天为黄色提示，400天为红色提示，可以自己定义。
-    let warningDay = 200;
-    let errorDay = 400;
+    let warningDay = 180;
+    let errorDay = 365;
     // 确保能够获取到文章时间以及在文章详情页
     let times = document.getElementsByTagName('time');
     if (times.length === 0) { return; }
@@ -16,12 +16,12 @@
         /* 发布时间超过指定时间（毫秒） */
         //note warning 以及 note danger 是 Next 主题的自定义模板语法，如果使用其他主题，请自行更改样式以达到最佳显示效果
     if (interval > warningDay * 3600 * 24 * 1000 && interval < errorDay * 3600 * 24 * 1000) {
-        posts[0].innerHTML = '<div class="note warning">' +
-            '<h5>文章时效性提示</h5><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。</p>' +
+        posts[0].innerHTML = '<div class="admonition warning">' +
+            '<p class="admonition-title">文章时效性提示</p> <p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。</p>' +
             '</div>' + posts[0].innerHTML;
     } else if (interval >= errorDay * 3600 * 24 * 1000) {
-        posts[0].innerHTML = '<div class="note danger">' +
-            '<h5>文章时效性提示</h5><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。</p>' +
+        posts[0].innerHTML = '<div class="admonition error">' +
+            '<p class="admonition-title">文章时效性提示</p> <p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。</p>' +
             '</div>' + posts[0].innerHTML;
     }
 })();
